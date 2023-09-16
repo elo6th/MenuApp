0f031a3b7257f70c262b7e71bc02a08708365d8f
elo6th <126991910+elo6th@users.noreply.github.com>
2023-09-17 01:09:59 +0200

    Create MenuView.swift

---
+++ MenuView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Menu
+//
+//  Created by Ozan Kocakaya on 11.09.23.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    @State var menuItems:[MenuItem] = [MenuItem]()
+    var dataService = DataService()
+    
+    var body: some View {
+        List(menuItems) { item in
+            MenuListRow(item: item)
+        }
+        .listStyle(.plain)
+        .onAppear {
+            // Call for the data
+            menuItems = dataService.getData()
+        }
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
